c641944b1d0efc8aa2c8a71358d36d7b546f653f
vucodeverse <vunguyen111105@gmail.com>
2026-01-31 21:52:43 +0700

    Create function create user

--- src/main/java/edu/fpt/groupfive/service/UserService.java
+++ src/main/java/edu/fpt/groupfive/service/UserService.java
@@ -1,4 +1,7 @@
 package edu.fpt.groupfive.service;
 
+import edu.fpt.groupfive.dto.request.UseCreateRequest;
+
 public interface UserService {
+    void createUser(UseCreateRequest request);
 }
---
+++ src/main/java/edu/fpt/groupfive/service/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package edu.fpt.groupfive.service.impl;
+
+import edu.fpt.groupfive.dao.UserDAO;
+import edu.fpt.groupfive.dto.request.UseCreateRequest;
+import edu.fpt.groupfive.model.Users;
+import edu.fpt.groupfive.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    @Override
+    public void createUser(UseCreateRequest request) {
+        if (userDAO.existsByUsername(request.getUsername())) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        Users user = new Users();
+        user.setUsername(request.getUsername());
+        user.setPasswordHash(request.getPassword());
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setRole(request.getRole());
+        user.setStatus("ACTIVE");
+        user.setCreatedDate(LocalDateTime.now());
+        user.setDepartmentId(request.getDepartmentId());
+
+        userDAO.insert(user);
+    }
+}
