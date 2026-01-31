a5706b44493f75ceb0d508ec51a7135a0db0f60d
vucodeverse <vunguyen111105@gmail.com>
2026-01-31 15:43:26 +0700

    Create dao department request & add feature insert, update, delete, exit name

--- src/main/java/edu/fpt/groupfive/dao/DepartmentDAO.java
+++ src/main/java/edu/fpt/groupfive/dao/DepartmentDAO.java
@@ -2,6 +2,19 @@ package edu.fpt.groupfive.dao;
 
 import edu.fpt.groupfive.model.Department;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DepartmentDAO {
-    Department save(Department department);
+    void insert(Department department);
+
+    void update(Department department);
+
+    void delete(Integer departmentId);
+
+    Optional<Department> findById(Integer departmentId);
+
+    List<Department> findAll();
+
+    boolean existsByName(String departmentName);
 }
--- src/main/java/edu/fpt/groupfive/dao/impl/DepartmentDAOImpl.java
+++ src/main/java/edu/fpt/groupfive/dao/impl/DepartmentDAOImpl.java
@@ -1,28 +1,108 @@
 package edu.fpt.groupfive.dao.impl;
 
+import edu.fpt.groupfive.config.DatabaseConfig;
 import edu.fpt.groupfive.dao.DepartmentDAO;
 import edu.fpt.groupfive.model.Department;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class DepartmentDAOImpl implements DepartmentDAO {
 
-    private final DataSource dataSource;
+    @Override
+    public void insert(Department department) {
+        String query = """
+                INSERT INTO Departments
+                    (department_name, created_date, status, manager_user_id)
+                VALUES (?, ?, ?, ?)
+                """;
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)
+                ) {
+            preparedStatement.setString(1, department.getDepartmentName());
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(department.getCreatedDate()));
+            preparedStatement.setString(3, department.getStatus());
+            preparedStatement.setObject(4, department.getManagerId());
 
-    public  DepartmentDAOImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-    private Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
     }
 
     @Override
-    public Department save(Department department) {
-        return null;
+    public void update(Department department) {
+        String sql = """
+            UPDATE Departments
+            SET department_name = ?,
+                updated_date = ?,
+                status = ?,
+                manager_user_id = ?
+            WHERE department_id = ?
+        """;
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setString(1, department.getDepartmentName());
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(department.getUpdatedDate()));
+            preparedStatement.setString(3, department.getStatus());
+            preparedStatement.setObject(4, department.getManagerId());
+            preparedStatement.setInt(5, department.getDepartmentId());
+
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(Integer departmentId) {
+        String sql = """
+            DELETE FROM Departments WHERE department_id = ?
+        """;
+
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setInt(1, departmentId);
+
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Optional<Department> findById(Integer departmentId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public boolean existsByName(String departmentName) {
+        String sql = "SELECT 1 FROM Departments WHERE department_name = ?";
+
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setString(1, departmentName);
+            return ps.executeQuery().next();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
