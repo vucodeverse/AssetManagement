7d29242f66746f689446384c768fe9f054ae1052
vucodeverse <vunguyen111105@gmail.com>
2026-01-31 15:45:41 +0700

    Test service, connect database

--- src/main/java/edu/fpt/groupfive/Main.java
+++ src/main/java/edu/fpt/groupfive/Main.java
@@ -1,6 +1,10 @@
 package edu.fpt.groupfive;
 
 import edu.fpt.groupfive.config.DatabaseConfig;
+import edu.fpt.groupfive.dao.DepartmentDAO;
+import edu.fpt.groupfive.dao.impl.DepartmentDAOImpl;
+import edu.fpt.groupfive.dto.request.DepartmentCreateRequest;
+import edu.fpt.groupfive.service.impl.DepartmentServiceImpl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,5 +22,14 @@ public class Main {
             System.out.println("❌ LỖI KẾT NỐI DATABASE!");
             e.printStackTrace();
         }
+
+        DepartmentCreateRequest request = new DepartmentCreateRequest("Information Technology Department", null);
+
+        DepartmentDAO departmentDAO = new DepartmentDAOImpl();
+        DepartmentServiceImpl service = new DepartmentServiceImpl(departmentDAO);
+
+        service.createDepartment(request);
+
+
     }
 }
