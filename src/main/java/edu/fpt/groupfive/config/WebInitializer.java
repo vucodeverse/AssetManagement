31642a56a1419f85e64897119f8ee230e1e699fe
vucodeverse <vunguyen111105@gmail.com>
2026-02-01 00:12:03 +0700

    Config thymeleaf and Config read css, js

---
+++ src/main/java/edu/fpt/groupfive/config/WebInitializer.java
@@ -0,0 +1,22 @@
+package edu.fpt.groupfive.config;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+}
