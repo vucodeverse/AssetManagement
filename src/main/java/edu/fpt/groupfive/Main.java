262ec886d5c82bdd707d8f2dc99243659dcf7957
vucodeverse <vunguyen111105@gmail.com>
2026-02-03 09:53:40 +0700

    Initial Commit

--- src/main/java/edu/fpt/groupfive/Main.java
+++ src/main/java/edu/fpt/groupfive/Main.java
@@ -17,27 +17,28 @@ import java.sql.PreparedStatement;
 
 public class Main {
     public static void main(String[] args) {
-        testDatabaseConnection();
+        //testDatabaseConnection();
+        System.out.println("Hello world");
     }
 
-    private static void testDatabaseConnection() {
-        DatabaseConfig databaseConfig = new DatabaseConfig();
-
-        try (Connection conn = databaseConfig.getConnection()) {
-
-            if (conn != null && !conn.isClosed()) {
-                System.out.println("✅ KẾT NỐI DATABASE THÀNH CÔNG!");
-                System.out.println("➡️ URL đang kết nối: " + conn.getMetaData().getURL());
-                System.out.println("➡️ Username: " + conn.getMetaData().getUserName());
-            } else {
-                System.out.println("❌ KẾT NỐI DATABASE THẤT BẠI!");
-            }
-
-        } catch (Exception e) {
-            System.out.println("❌ LỖI KẾT NỐI DATABASE!");
-            e.printStackTrace();
-        }
-    }
+//    private static void testDatabaseConnection() {
+//        DatabaseConfig databaseConfig = new DatabaseConfig();
+//
+//        try (Connection conn = databaseConfig.getConnection()) {
+//
+//            if (conn != null && !conn.isClosed()) {
+//                System.out.println("✅ KẾT NỐI DATABASE THÀNH CÔNG!");
+//                System.out.println("➡️ URL đang kết nối: " + conn.getMetaData().getURL());
+//                System.out.println("➡️ Username: " + conn.getMetaData().getUserName());
+//            } else {
+//                System.out.println("❌ KẾT NỐI DATABASE THẤT BẠI!");
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println("❌ LỖI KẾT NỐI DATABASE!");
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
