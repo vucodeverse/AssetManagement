08e4a563ca42360a5ef719cc2632cafa00f75d06
vucodeverse <vunguyen111105@gmail.com>
2026-02-24 16:29:23 +0700

    Initial commit

--- src/main/java/edu/fpt/groupfive/util/config/database/DatabaseConfig.java
+++ src/main/java/edu/fpt/groupfive/util/config/database/DatabaseConfig.java
@@ -1,8 +1,11 @@
 package edu.fpt.groupfive.util.config.database;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -21,6 +24,7 @@ public class DatabaseConfig {
     @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
 
+
     public Connection getConnection() {
         try {
             Class.forName(driverClassName);
@@ -29,4 +33,14 @@ public class DatabaseConfig {
             throw new RuntimeException("Cannot connect to database", e);
         }
     }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource ds = new DriverManagerDataSource();
+//        ds.setDriverClassName(driverClassName);
+//        ds.setUrl(url);
+//        ds.setUsername(username);
+//        ds.setPassword(password);
+//        return ds;
+//    }
 }
