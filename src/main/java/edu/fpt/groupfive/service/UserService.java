5c2d6b8d97679c7f968ff275ad39805a9cab0d12
vucodeverse <vunguyen111105@gmail.com>
2026-02-24 16:31:15 +0700

    Complete search, filter

--- src/main/java/edu/fpt/groupfive/service/UserService.java
+++ src/main/java/edu/fpt/groupfive/service/UserService.java
@@ -1,8 +1,10 @@
 package edu.fpt.groupfive.service;
 
+import edu.fpt.groupfive.common.Role;
 import edu.fpt.groupfive.dto.request.UserCreateRequest;
 import edu.fpt.groupfive.dto.request.UserUpdateRequest;
 import edu.fpt.groupfive.dto.response.UserResponse;
+import edu.fpt.groupfive.model.Users;
 
 import java.util.List;
 
@@ -10,6 +12,19 @@ public interface UserService {
     void createUser(UserCreateRequest request);
     void updateUser(UserUpdateRequest request);
     void removeUser(Integer id);
-    List<UserResponse> getAllUsers();
+    List<UserResponse> searchUsers(
+            int page, int size,
+            String status, Integer departmentId,
+            Role role, String keyword);
+
+    int getTotalPagesWithFilter(
+            int size,
+            String status, Integer departmentId,
+            Role role, String keyword);
     UserResponse getUserById(Integer id);
+    List<UserResponse> getAllUserByDepartId(Integer departId);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
+
 }
