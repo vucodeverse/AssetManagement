442e168c45c4c8d4d68aef2799647c1192ba7358
vucodeverse <vunguyen111105@gmail.com>
2026-02-24 16:31:08 +0700

    Complete search, filter

--- src/main/java/edu/fpt/groupfive/service/impl/UserServiceImpl.java
+++ src/main/java/edu/fpt/groupfive/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package edu.fpt.groupfive.service.impl;
 
+import edu.fpt.groupfive.common.Role;
+import edu.fpt.groupfive.dao.DepartmentDAO;
 import edu.fpt.groupfive.dao.UserDAO;
 import edu.fpt.groupfive.dto.request.UserCreateRequest;
 import edu.fpt.groupfive.dto.request.UserUpdateRequest;
 import edu.fpt.groupfive.dto.response.UserResponse;
 import edu.fpt.groupfive.mapper.UserMapper;
+import edu.fpt.groupfive.model.Department;
 import edu.fpt.groupfive.model.Users;
 import edu.fpt.groupfive.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,54 +25,101 @@ public class UserServiceImpl implements UserService {
     private final UserDAO userDAO;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final DepartmentDAO departmentDAO;
 
+    //Kiểm tra xem phòng có trưởng phòng hay chưa
+    private void existsDepartmentMgr(Integer departmentId, Integer userId) {
 
-    @Override
-    public void createUser(UserCreateRequest request) {
-        if (userDAO.existsByUsername(request.getUsername())) {
-            throw new IllegalArgumentException("Username already exists");
+        // Lấy department theo id
+        Department department = departmentDAO.findById(departmentId)
+                .orElseThrow(() -> new RuntimeException("Department not found"));
+
+        // Nếu đã có manager khác
+        if (department.getManagerId() != null
+                && !department.getManagerId().equals(userId)) {
+
+            throw new IllegalArgumentException("Department already has a manager");
         }
-
-        if (request.getEmail() != null &&
-                userDAO.existsByEmail(request.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
-        }
-
-        Users user = userMapper.toUser(request);
-        user.setPasswordHash(passwordEncoder.encode(request.getPassword()));
-        user.setStatus("ACTIVE");
-        user.setCreatedDate(LocalDateTime.now());
-
-        userDAO.insert(user);
     }
 
+
+    // Tạo một user mới
+    @Override
+    public void createUser(UserCreateRequest request) {
+
+        // Map dữ liệu từ request qua model
+        Users user = userMapper.toUser(request);
+        // Mã hóa & Lưu mật khẩu
+        user.setPasswordHash(passwordEncoder.encode(request.getPassword()));
+        // Mặc định trạnh thái khi tạo
+        user.setStatus("ACTIVE");
+        // Lấy thời gian hiện tại
+        user.setCreatedDate(LocalDateTime.now());
+
+        // Lưu user xuống database
+        userDAO.insert(user);
+
+        // Nếu vai trò là trưởng phòng cập nhật trưởng phòng cho phòng đấy luôn
+        if (user.getRole() == Role.DEPARTMENT_MANAGER) {
+            departmentDAO.updateManager(user.getDepartmentId(), user.getUserId());
+        }
+    }
+
+
+    // Update thông tin của user
     @Override
     public void updateUser(UserUpdateRequest request) {
-        //Lấy user theo id
+        // Lấy user theo id
         Users existing = userDAO.findById(request.getUserId())
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        //Kiểm tra email có tồn tại chưa
-        if (request.getEmail() != null
-                && !request.getEmail().equals(existing.getEmail())
-                && userDAO.existsByEmail(request.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
-        }
+        Role oldRole = existing.getRole();
+        Integer oldDepartmentId = existing.getDepartmentId();
 
+        // Map dữ liệu từ request qua model
         userMapper.updateUserFromRequest(request, existing);
 
         // Chỉ đổi password nếu nhập mới
         if (request.getPassword() != null && !request.getPassword().isBlank()) {
-            existing.setPasswordHash(
-                    passwordEncoder.encode(request.getPassword())
-            );
+            existing.setPasswordHash(passwordEncoder.encode(request.getPassword()));
         }
 
+        // Lưu thời gian cập nhật
         existing.setUpdatedDate(LocalDateTime.now());
+
+
+        // Nếu trước là manager trong một phòng mà giờ không còn
+        if (oldRole == Role.DEPARTMENT_MANAGER && existing.getRole() != Role.DEPARTMENT_MANAGER) {
+            departmentDAO.updateManager(oldDepartmentId, null);
+        }
+
+
+        // Nếu trước không phải manager mà giờ là manager của phòng
+        if (oldRole != Role.DEPARTMENT_MANAGER && existing.getRole() == Role.DEPARTMENT_MANAGER) {
+            departmentDAO.updateManager(existing.getDepartmentId(), existing.getUserId());
+        }
+
+        // Nếu vẫn là manager nhưng đổi phòng
+        if (oldRole == Role.DEPARTMENT_MANAGER
+                && existing.getRole() == Role.DEPARTMENT_MANAGER
+                && !oldDepartmentId.equals(existing.getDepartmentId())) {
+
+            // Check phòng mới
+
+            // Xóa manager phòng cũ
+            departmentDAO.updateManager(oldDepartmentId, null);
+
+            // Set manager phòng mới
+            departmentDAO.updateManager(existing.getDepartmentId(), existing.getUserId());
+        }
+
+        // Cập nhật thông tin
         userDAO.update(existing);
+
     }
 
 
+    // Remove một user (Update status)
     @Override
     public void removeUser(Integer id) {
         Users user = userDAO.findById(id)
@@ -77,17 +128,53 @@ public class UserServiceImpl implements UserService {
         userDAO.delete(id);
     }
 
+
+    // Tìm kiếm user theo keyword
     @Override
-    public List<UserResponse> getAllUsers() {
-        List<Users> users = userDAO.findAll();
+    public List<UserResponse> searchUsers(
+            int page, int size, String status,
+            Integer departmentId, Role role, String keyword) {
+
+        int offset = page * size;
+
+        List<Users> users = userDAO.searchUsers(offset, size, status,
+                departmentId, role, keyword);
+
         return userMapper.toResponseList(users);
     }
+
+    @Override
+    public int getTotalPagesWithFilter(
+            int size, String status, Integer departmentId,
+            Role role, String keyword) {
+
+        int total = userDAO.countUsersWithFilter(status, departmentId, role, keyword);
+
+        return (int) Math.ceil((double) total / size);
+    }
+
 
     @Override
     public UserResponse getUserById(Integer id) {
         Users user = userDAO.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
         return userMapper.toResponse(user);
+    }
+
+    @Override
+    public List<UserResponse> getAllUserByDepartId(Integer departId) {
+        List<Users> list = userDAO.findByDepartmentId(departId);
+        return userMapper.toResponseList(list);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userDAO.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userDAO.existsByEmail(email);
     }
 
 
