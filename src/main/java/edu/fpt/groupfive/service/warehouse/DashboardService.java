91e71e5b6ee88bed84ea5b83a2d4ef7b4b288bf5
Phạm Gia Khánh <me.khanhphm@gmail.com>
2026-03-10 16:26:41 +0700

    feat(dashboard): add dashboard and fix some small config

--- src/main/java/edu/fpt/groupfive/service/warehouse/DashboardService.java
+++
@@ -1,7 +0,0 @@
-package edu.fpt.groupfive.service.warehouse;
-
-import edu.fpt.groupfive.dto.warehouse.response.DashboardMetricsDto;
-
-public interface DashboardService {
-    DashboardMetricsDto getDashboardData(Integer managerUserId);
-}
