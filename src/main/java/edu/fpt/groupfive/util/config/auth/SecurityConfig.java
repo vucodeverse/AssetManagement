5cd37b382035a21eb4cbca7b7b40070f1430397c
HungNM <lalala123hung@gmail.com>
2026-03-12 11:14:26 +0700

    Modify: Update Logout done

--- src/main/java/edu/fpt/groupfive/util/config/auth/SecurityConfig.java
+++ src/main/java/edu/fpt/groupfive/util/config/auth/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.SessionMan
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -81,7 +82,8 @@ public class SecurityConfig {
                                                 .failureHandler(customerAuthenticationFailureHandler)
                                                 .permitAll())
                                 .logout(l -> l.logoutUrl("/logout")
-                                                .logoutSuccessUrl("/login?logout=true")
+                                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                                                .logoutSuccessUrl("/auth/login?logout=true")
                                                 .invalidateHttpSession(true)
                                                 .deleteCookies("JSESSIONID")
                                                 .permitAll())
