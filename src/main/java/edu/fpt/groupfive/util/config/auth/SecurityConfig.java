02b282153466410c8bc19ea2a5e1a6eb3a7f87e0
vucodeverse <vunguyen111105@gmail.com>
2026-03-20 20:34:14 +0700

    Config security

--- src/main/java/edu/fpt/groupfive/util/config/auth/SecurityConfig.java
+++ src/main/java/edu/fpt/groupfive/util/config/auth/SecurityConfig.java
@@ -22,91 +22,91 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-        private final CustomerUserDetailsService userDetailsService;
-        private final CustomerAuthenticationFailureHandler customerAuthenticationFailureHandler;
-        private final CustomerAuthenticationSuccessHandler customerAuthenticationSuccessHandler;
+    private final CustomerUserDetailsService userDetailsService;
+    private final CustomerAuthenticationFailureHandler customerAuthenticationFailureHandler;
+    private final CustomerAuthenticationSuccessHandler customerAuthenticationSuccessHandler;
 
-        @Value("${app.security.enabled}")
-        private boolean securityEnable;
+    @Value("${app.security.enabled}")
+    private boolean securityEnable;
 
-        // các url dc truy cập tự do
-        private final String[] WHITE_LIST = { "/auth/login", "/static/**" };
+    // các url dc truy cập tự do
+    private final String[] WHITE_LIST = {"/auth/login", "/static/**"};
 
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return new BCryptPasswordEncoder(10);
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // tắt security
+        if (!securityEnable) {
+            httpSecurity
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            return httpSecurity.build();
         }
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // bật bth
+        httpSecurity
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                                .requestMatchers(WHITE_LIST)
+                                .permitAll()
+                                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                                .requestMatchers("/department/**")
+                                .hasAnyAuthority("DEPARTMENT_MANAGER", "ADMIN")
+                                // Domain URLs: không gắn với role
+                                .requestMatchers("/purchases/**")
+                                .hasAnyAuthority("ASSET_MANAGER", "DIRECTOR",
+                                        "PURCHASE_STAFF", "ASSET-MANAGER",
+                                        "ADMIN")
+                                .requestMatchers("/quotations/**")
+                                .hasAnyAuthority("PURCHASE_STAFF", "DIRECTOR", "ADMIN")
+                                .requestMatchers("/purchase-orders/**")
+                                .hasAnyAuthority("PURCHASE_STAFF", "DIRECTOR", "ADMIN")
+                                // Role dashboards: gắn với role
+                                .requestMatchers("/director/**")
+                                .hasAnyAuthority("DIRECTOR", "ADMIN")
+                                .requestMatchers("/purchase-staff/**")
+                                .hasAnyAuthority("PURCHASE_STAFF", "ADMIN")
+                                .requestMatchers("/asset-manager/**")
+                                .hasAnyAuthority("ASSET_MANAGER", "ADMIN")
+                                .anyRequest()
+                                .authenticated())
+                .authenticationProvider(authenticationProvider())
+                .formLogin(f -> f.loginPage("/auth/login")
+                        .loginProcessingUrl("/login")
+                        .successHandler(customerAuthenticationSuccessHandler)
+                        .failureHandler(customerAuthenticationFailureHandler)
+                        .permitAll())
+                .logout(l -> l.logoutUrl("/logout")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                        .logoutSuccessUrl("/auth/login?logout=true")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll())
+                .sessionManagement(
+                        s -> s.sessionFixation(
+                                        SessionManagementConfigurer.SessionFixationConfigurer::newSession)
+                                .maximumSessions(1).maxSessionsPreventsLogin(false));
 
-                // tắt security
-                if (!securityEnable) {
-                        httpSecurity
-                                        .csrf(csrf -> csrf.disable())
-                                        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-                        return httpSecurity.build();
-                }
+        return httpSecurity.build();
+    }
 
-                // bật bth
-                httpSecurity
-                                .authorizeHttpRequests(
-                                                authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
-                                                                .requestMatchers(WHITE_LIST)
-                                                                .permitAll()
-                                                                .requestMatchers("/admin/**").hasAuthority("ADMIN")
-                                                                .requestMatchers("/dept-manager/**")
-                                                                .hasAnyAuthority("DEPARTMENT_MANAGER", "ADMIN")
-                                                                // Domain URLs: không gắn với role
-                                                                .requestMatchers("/purchases/**")
-                                                                .hasAnyAuthority("ASSET_MANAGER", "DIRECTOR",
-                                                                                "PURCHASE_STAFF","ASSET-MANAGER" ,
-                                                                        "ADMIN")
-                                                                .requestMatchers("/quotations/**")
-                                                                .hasAnyAuthority("PURCHASE_STAFF", "DIRECTOR", "ADMIN")
-                                                                .requestMatchers("/purchase-orders/**")
-                                                                .hasAnyAuthority("PURCHASE_STAFF", "DIRECTOR", "ADMIN")
-                                                                // Role dashboards: gắn với role
-                                                                .requestMatchers("/director/**")
-                                                                .hasAnyAuthority("DIRECTOR", "ADMIN")
-                                                                .requestMatchers("/purchase-staff/**")
-                                                                .hasAnyAuthority("PURCHASE_STAFF", "ADMIN")
-                                                                .requestMatchers("/asset-manager/**")
-                                                                .hasAnyAuthority("ASSET_MANAGER", "ADMIN")
-                                                                .anyRequest()
-                                                                .authenticated())
-                                .authenticationProvider(authenticationProvider())
-                                .formLogin(f -> f.loginPage("/auth/login")
-                                                .loginProcessingUrl("/login")
-                                                .successHandler(customerAuthenticationSuccessHandler)
-                                                .failureHandler(customerAuthenticationFailureHandler)
-                                                .permitAll())
-                                .logout(l -> l.logoutUrl("/logout")
-                                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                                                .logoutSuccessUrl("/auth/login?logout=true")
-                                                .invalidateHttpSession(true)
-                                                .deleteCookies("JSESSIONID")
-                                                .permitAll())
-                                .sessionManagement(
-                                                s -> s.sessionFixation(
-                                                                SessionManagementConfigurer.SessionFixationConfigurer::newSession)
-                                                                .maximumSessions(1).maxSessionsPreventsLogin(false));
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
 
-                return httpSecurity.build();
-        }
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 
-        @Bean
-        public AuthenticationProvider authenticationProvider() {
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
-                DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        // hiển thị lỗi
+        daoAuthenticationProvider.setHideUserNotFoundExceptions(false);
 
-                daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-                daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-
-                // hiển thị lỗi
-                daoAuthenticationProvider.setHideUserNotFoundExceptions(false);
-
-                return daoAuthenticationProvider;
-        }
+        return daoAuthenticationProvider;
+    }
 
 }
