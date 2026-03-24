1489d86757b87b7448a7c8fc148bf02e3569655c
HungNM <lalala123hung@gmail.com>
2026-03-24 09:59:05 +0700

    Modify: fix UI condition

--- src/main/java/edu/fpt/groupfive/util/config/auth/CustomerAuthenticationSuccessHandler.java
+++ src/main/java/edu/fpt/groupfive/util/config/auth/CustomerAuthenticationSuccessHandler.java
@@ -21,7 +21,8 @@ public class CustomerAuthenticationSuccessHandler implements AuthenticationSucce
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException, ServletException {
+
 
         // Lấy username từ authentication
         String username = authentication.getName();
@@ -36,60 +37,32 @@ public class CustomerAuthenticationSuccessHandler implements AuthenticationSucce
         session.setAttribute("fullName", user.getFullName());
         session.setAttribute("role", user.getRole().name());
 
+
         String redirectUrl = request.getContextPath();
 
         // nếu role là director
         if (authentication.getAuthorities().stream()
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("DIRECTOR"))) {
             redirectUrl += "/director/dashboard";
-        }
-
-        if (authentication.getAuthorities().stream()
+        } else if (authentication.getAuthorities().stream()
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("PURCHASE_STAFF"))) {
             redirectUrl += "/purchase-staff/dashboard";
-        }
-
-        // if (authentication.getAuthorities().stream().
-        // anyMatch(grantedAuthority ->
-        // grantedAuthority.getAuthority().equals("ADMIN"))) {
-        // if (authentication.getAuthorities().stream()
-        // .anyMatch(grantedAuthority ->
-        // grantedAuthority.getAuthority().equals("ADMIN"))) {
-        // redirectUrl += "/admin/home";
-        // }
-//        if (authentication.getAuthorities().stream().
-//                anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ADMIN"))) {
-//            if (authentication.getAuthorities().stream()
-//                    .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ADMIN"))) {
-//                redirectUrl += "/admin/home";
-//            }
-
-        if (authentication.getAuthorities().stream()
+        } else if (authentication.getAuthorities().stream()
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("WAREHOUSE_STAFF"))) {
             redirectUrl += "/wh/dashboard";
-        }
-
-        if (authentication.getAuthorities().stream()
+        } else if (authentication.getAuthorities().stream()
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ADMIN"))) {
-            redirectUrl += "/admin/dashboard";
-        }
-
-        if (authentication.getAuthorities().stream()
+            redirectUrl += "/admin/users";
+        } else if (authentication.getAuthorities().stream()
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("DEPARTMENT_MANAGER"))) {
             redirectUrl += "/department/allocation-request/list";
-        }
-        if (authentication.getAuthorities().stream()
+        } else if (authentication.getAuthorities().stream()
                 .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ASSET_MANAGER"))) {
             redirectUrl += "/manager/dashboard";
-        }
-
-        if (authentication.getAuthorities().stream()
-                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ASSET_MANAGER"))) {
-            redirectUrl += "/asset-manager/allocation-request/list";
+        } else {
+            redirectUrl += "/";
         }
 
         response.sendRedirect(redirectUrl);
-            response.sendRedirect(redirectUrl);
-//        }
     }
 }
